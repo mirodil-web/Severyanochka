7db742d54f0629c637fceb9c82dc5a8b38ea44d1
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2023-08-31 10:48:08 +0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header>
+          <div className='container'>
+              
+          </div>
+    </header>
+  )
+}
+
+export default Header
